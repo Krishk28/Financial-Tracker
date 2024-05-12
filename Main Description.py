ae2305b8998658cf94d0b82ec0214f2de1816b27
Krishk28 <Krish.Kumar2163@gmail.com>
2024-05-13 11:01:16 +1200

    Creating an App Frame and its configuration

--- Main Description.py
+++ Main Description.py
@@ -5,3 +5,10 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
 import pandas as pd
+
+#Our app Frame
+app = customtkinter.CTk()
+app.title("Financial Tracker")
+app.configure(bg='green')
+app.minsize(1920, 1080)  # Set a minimum size larger than the enforced minimum
+app.attributes("-fullscreen", True)
