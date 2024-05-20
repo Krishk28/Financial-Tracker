fe0467a5c0092ce7662650c53d31285de65dd758
Krishk28 <Krish.Kumar2163@gmail.com>
2024-05-21 09:57:10 +1200

    sorting code

--- Main Description.py
+++ Main Description.py
@@ -1,13 +1,11 @@
-import tkinter
-import customtkinter 
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
 import pandas as pd
+import customtkinter
 
-
-#Our app Frame
+# Our app Frame
 app = customtkinter.CTk(fg_color="#0b1024")
 app.title("Financial Tracker")
 app.minsize(app.winfo_screenwidth(), app.winfo_screenheight())
@@ -15,9 +13,9 @@ app.state('zoomed')
 app.attributes('-fullscreen', True)
 
 
-#creating a frame for my widgets
-Directions_Frame = customtkinter.CTkFrame(app,corner_radius=25, fg_color="#181a2f")
-Directions_Frame.grid(row= 0, column= 0, pady=15, padx=20, ipady=4)
+# Creating a frame for my widgets
+Directions_Frame = customtkinter.CTkFrame(app, corner_radius=25, fg_color="#181a2f")
+Directions_Frame.grid(row=0, column=0, pady=15, padx=20, ipady=4)
 
 # Configure rows and columns to expand and fill the available space
 app.rowconfigure(0, weight=1)
@@ -25,9 +23,8 @@ app.columnconfigure(0, weight=1)
 Directions_Frame.rowconfigure(0, weight=1)
 Directions_Frame.columnconfigure(0, weight=1)
 
-
-#Adding UI elements, Text Frame
-title = ttk.Label(Directions_Frame, text="Directions:", font=("", 30))
+# Adding UI elements, Text Frame
+title = ttk.Label(Directions_Frame, text="Directions:", font=("Inconsolata", 30))
 title.grid(row=3, column=4)
 
 
@@ -41,9 +38,9 @@ def insert_numbered_list():
     ]
     
     for idx, item in enumerate(items, start=1):
-        label = customtkinter.CTkLabel(Directions_Frame, text=f"{idx}.   {item}", font=("Canva Sans",19))
+        label = customtkinter.CTkLabel(Directions_Frame, text=f"{idx}.   {item}", font=("Inconsolata", 19))
         label.grid(row=idx+5, column=4, padx=20, pady=5)
-        label.configure(wraplength=600)  # Set wrap length to 400 pixels so words dont run on
+        label.configure(wraplength=600)  # Set wrap length to 400 pixels so words don't run on
 
 insert_numbered_list()
 
@@ -54,14 +51,11 @@ def import_csv_data():
     v.set(csv_file_path)
     #df = pd.read_csv(csv_file_path)
 
+# Import CSV Button
+ttk.Button(Directions_Frame, text='Upload CSV', command=import_csv_data, style='Custom.TButton').grid(row=13, column=4, ipady=4)
 
-#Import CSV Button
-ttk.Button(Directions_Frame, text='Upload CSV',command=import_csv_data,style='Custom.TButton').grid(row=13, column=4,ipady=4)
-
-#Import Enter Pay button
-ttk.Button(Directions_Frame, text='Enter Pay',style='Custom.TButton').grid(row=12, column=4)
-
-
+# Import Enter Pay button
+ttk.Button(Directions_Frame, text='Enter Pay', style='Custom.TButton').grid(row=12, column=4)
 
 # Run app
 app.mainloop()
