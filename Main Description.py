968de1e3ffd966682af648a2d41ec2a4020e6f55
Krishk28 <Krish.Kumar2163@gmail.com>
2024-05-27 11:14:40 +1200

    Updated Padding of buttons and recoloured my title

--- Main Description.py
+++ Main Description.py
@@ -27,7 +27,7 @@ Directions_Frame.columnconfigure(0, weight=1)
 # Adding UI elements, Text Frame
 title = customtkinter.CTkLabel(Directions_Frame, text="Directions:", font=("", 30))
 title.grid(row=3, column=4)
-header = customtkinter.CTkLabel(app, text="BudgetBuddy", font=("", 50), fg_color="white")
+header = customtkinter.CTkLabel(app, text="BudgetBuddy", font=("", 50), text_color="white")
 header.grid(row=0, column=0, sticky="nw", padx = 20, pady=20)
 
 
@@ -66,15 +66,17 @@ def openNewWindow():
 
 # Import CSV Button
 customtkinter.CTkButton(Directions_Frame, text='Upload CSV', command=import_csv_data,
+    corner_radius=30,
     border_width=1,
     border_color="black",
-    fg_color="transparent").grid(row=13, column=4,pady=5)
+    fg_color="transparent").grid(row=13, column=4, pady=7, ipady=2)
 
 # Import Enter Pay button
-customtkinter.CTkButton(Directions_Frame, text='Enter Pay', corner_radius=30, command=openNewWindow,
+customtkinter.CTkButton(Directions_Frame, text='Enter Pay', command=openNewWindow,
+    corner_radius=30,
     border_width=1,
     border_color="black",
-    fg_color="transparent").grid(row=12, column=4, pady=5)
+    fg_color="transparent").grid(row=12, column=4, ipady=2)
 
 # Run app
 app.mainloop()
